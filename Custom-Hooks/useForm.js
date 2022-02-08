c02c3d29f70177315287725826f5e42d1d2a283f
Charles Kasasira <charleskasasira01@gmail.com>
2022-02-08 16:39:10 +0300

    useForm hook

---
+++ Custom-Hooks/useForm.js
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+
+export const useForm = (initialValues) => {
+    const [fields, setValues ] = useState(initialValues)
+    return [
+        fields,
+        event => {
+            const {id, value} = event.target
+            setValues({
+                ...fields,
+                [id]:value
+            })
+        }
+    ]
+}
